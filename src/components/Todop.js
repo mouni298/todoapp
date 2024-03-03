c43609e518abca9774818ff434f452c915e8dd46
Mounika Grandhi <grandhimounika29@gmail.com>
2024-03-03 15:54:13 +0530

    updating status of tasks and displaying tasks based on button selected

--- src/components/Todop.js
+++ src/components/Todop.js
@@ -6,13 +6,26 @@ class Todoapp extends Component{
         this.state = {
            tasks : [],
            inputValue : "",
+           buttonType : "all"
         }
         this.changeInput = this.changeInput.bind(this)
         this.addTask = this.addTask.bind(this)
         this.displayTasks = this.displayTasks.bind(this)
+        this.updateTaskStatus = this.updateTaskStatus.bind(this)
+        this.viewTasks = this.viewTasks.bind(this)
 
     }
+    updateTaskStatus=(index)=> event =>{
+        console.log(event,index)
+        let tasks = [...this.state.tasks]
+        tasks[index]['status'] = event.target.checked?'completed':'active'
+        this.setState({
+            tasks: tasks
+        },()=>console.log(this.state.tasks))
+    }
+
     addTask(){
+        if(this.state.inputValue !== ''){
         this.setState({
             tasks : [...this.state.tasks,{
                 name : this.state.inputValue,
@@ -25,13 +38,13 @@ class Todoapp extends Component{
             })
             console.log(this.state.tasks)}
         )
-
-    }s
+        }
+    }
     displayTasks(){
-        return this.state.tasks.map(task=>(
+        return this.state.tasks.map((task,index)=>((this.state.buttonType === "all" ?true:this.state.buttonType === task.status) &&
             <>
-            <input type="checkbox" id={task.name} checked={false}/>
-            <label htmlFor={task.name}>{task.name}</label>
+            <input type="checkbox" id={task.name} checked={task.status==='completed'} onChange={this.updateTaskStatus(index)}/>
+            <label htmlFor={task.name}>{task.name}</label><br/>
             </>
         ))
     }
@@ -42,13 +55,21 @@ class Todoapp extends Component{
             }
         )
     }
+    viewTasks(type){
+        this.setState({
+            buttonType : type
+        })
+    }
     render(){
         return(
                 <>
                 <h1>THINGS TO DO</h1>
-                <input type = "text" placeholder = "ADD NEW" onChange={this.changeInput} value={this.state.inputValue}/>
+                <input type = "text" placeholder = "ADD NEW" onChange={this.changeInput} value={this.state.inputValue}/><br/>
                 <button type ="submit" onClick={this.addTask}>+</button>
                 <h2>{this.displayTasks()}</h2>
+                <button type="submit" onClick={()=>this.viewTasks("all")}>All</button>
+                <button type="submit"onClick={()=>this.viewTasks("active")} >Active</button>
+                <button type="submit"onClick={()=>this.viewTasks("completed")} >Completed</button>
                 </>
         )
     }
